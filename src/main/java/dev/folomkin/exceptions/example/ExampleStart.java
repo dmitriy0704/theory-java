9ce4b9ace14124bb8675aff18016008abcb439b1
Dmitriy Folomkin <dmitriy.folomkin@gmail.com>
2024-06-24 18:30:15 +0300

    2024-06-24.Тема: Обработка исключений по книге Java для начинающих, Шилдт.

--- src/main/java/dev/folomkin/exceptions/example/ExampleStart.java
+++ src/main/java/dev/folomkin/exceptions/example/ExampleStart.java
@@ -2,17 +2,18 @@ package dev.folomkin.exceptions.example;
 
 public class ExampleStart {
     public static void main(String[] args) {
-        ExceptionExample.example(7, 7);
+            ExceptionExample.example(7, 7);
     }
 }
 
 class ExceptionExample {
-    static void example(int i, int y) {
+    static void example(int i, int y){
         try {
             int[] arr = new int[5];
             arr[i] = y;
-        } catch (Exception e) {
+        } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Exception: " + e.getMessage());
         }
+        System.out.println("После оператора catch...");
     }
 }
