78a0c089678aead5b0873b2e261f9358959ba61d
Dmitriy Folomkin <dmitriy.folomkin@gmail.com>
2024-07-01 16:59:45 +0300

    Add Test

--- src/test/java/dev/folomkin/oop/optionalpkg/example/ExampleStartTest.java
+++ src/test/java/dev/folomkin/oop/optionalpkg/example/ExampleStartTest.java
@@ -1,16 +1,16 @@
 package dev.folomkin.oop.optionalpkg.example;
 
 import org.junit.jupiter.api.Test;
-
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ExampleStartTest {
-    @Test()
+    @Test
     public void givenNonNull_whenCreatesNonNullable_thenCorrect() {
-        String name = "Wine";
-        Optional<String> opt = Optional.of(name);
-        assertTrue(opt.isPresent());
+        String name = null;
+        String opt = Optional.ofNullable(name).orElse("Dmitriy");
+        assertEquals("Dmitriy", opt);
+
     }
 }
