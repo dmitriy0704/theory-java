d3f81fcb49841ec2cd788eab6fe2682c1ee9ca0c
Dmitriy Folomkin <dmitriy.folomkin@gmail.com>
2024-07-01 13:08:03 +0300

    2024-07-01. Optional

--- src/main/java/dev/folomkin/oop/optionalpkg/example/OrderAction.java
+++ src/main/java/dev/folomkin/oop/optionalpkg/example/OrderAction.java
@@ -1,22 +1,12 @@
 package dev.folomkin.oop.optionalpkg.example;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class OrderAction {
     public Optional<Order> findById(List<Order> orders, long id) {
-
-        Order order = null;
-        List<Order> result = orders.stream()
-                .filter(o -> id == o.getOrderId()).toList();
-
-        //        if (result.size() != 0) {
-        //            order = orders.get(0);
-        //        }
-
-        Optional<Order> optionalResult = result.size() != 0 ? Optional.of(result.get(0)) : Optional.empty();
-
+        Optional<Order> optionalResult = orders.stream()
+                .filter(o -> id == o.getOrderId()).findAny();
         return optionalResult;
     }
 }
@@ -37,5 +27,25 @@ class Order {
 
     public void setOrderId(Long orderId) {
         this.orderId = orderId;
+    }
+}
+
+class OrderMain {
+    public static void main(String[] args) {
+        List<Order> orders = new ArrayList<>();
+        orders.add(new Order(1L));
+        orders.add(new Order(3L));
+        orders.add(new Order(4L));
+        OrderAction action = new OrderAction();
+        Optional<Order> optionalOrder = action.findById(orders, 1L);
+
+        if (optionalOrder.isPresent()) {
+            System.out.println(optionalOrder.get());
+        }
+
+        Set<Order> set = new HashSet<>();
+        optionalOrder.ifPresent(set::add);
+        System.out.println(set);
+
     }
 }
