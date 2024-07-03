2ae426f7e556c95101d6b1dec2ca644b44c3613a
Dmitriy Folomkin <dmitriy.folomkin@gmail.com>
2024-07-03 23:04:40 +0300

    Коллекции. List. Добалена запись в Wiki.

--- src/main/java/dev/folomkin/collections/example/ExampleStart.java
+++ src/main/java/dev/folomkin/collections/example/ExampleStart.java
@@ -1,31 +1,7 @@
 package dev.folomkin.collections.example;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
 public class ExampleStart {
     public static void main(String[] args) {
 
-        List<Integer> list = new ArrayList<>();
-
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(5);
-
-        Integer y = 3;
-        Iterator<Integer> iterator = list.iterator();
-//        while (iterator.hasNext()) {
-//            System.out.println(iterator.next());
-//        }
-
-        for (Integer i : list) {
-            System.out.println(i);
-        }
-
-
-        System.out.println(list);
     }
 }
