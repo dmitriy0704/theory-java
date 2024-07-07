c9de3465dbd43ef6c49a1c68c957b69f19f2c6fd
Dmitriy Folomkin <dmitriy.folomkin@gmail.com>
2024-07-07 20:15:07 +0300

    Коллекции...

--- src/main/java/dev/folomkin/collections/example/ExampleStart.java
+++ src/main/java/dev/folomkin/collections/example/ExampleStart.java
@@ -1,7 +1,19 @@
 package dev.folomkin.collections.example;
 
+import java.util.*;
+
 public class ExampleStart {
     public static void main(String[] args) {
 
+        Stack<String> stack = new Stack<>();
+        stack.push("A");
+        stack.push("B");
+        stack.push("C");
+        stack.push("D");
+        stack.push("E");
+
+        System.out.println(stack);
+        System.out.println(stack.peek());
+        System.out.println(stack);
     }
 }
