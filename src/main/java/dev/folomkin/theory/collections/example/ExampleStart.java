2fc1891e5a1faea4fdb9b9e022ba2608a7759d68
Dmitriy Folomkin <dmitriy.folomkin@gmail.com>
2024-07-24 13:25:16 +0300

    Сортровка массивов

--- src/main/java/dev/folomkin/theory/collections/example/ExampleStart.java
+++ src/main/java/dev/folomkin/theory/collections/example/ExampleStart.java
@@ -1,16 +1,18 @@
 package dev.folomkin.theory.collections.example;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 public class ExampleStart {
     public static void main(String[] args) {
 
-        Collection<String> collection = new ArrayList<>();
+        List<String> collection = new ArrayList<>();
         collection.add("A");
         collection.add("B");
         collection.add("C");
-
-        System.out.println(collection.add("D"));
+        System.out.println(collection.indexOf("C"));
+        System.out.println(collection);
     }
 }
--- src/main/java/dev/folomkin/theory/oop/example/ExampleStart.java
+++ src/main/java/dev/folomkin/theory/oop/example/ExampleStart.java
@@ -1,4 +1,13 @@
 package dev.folomkin.theory.oop.example;
 
+class Post<K extends Number, V>{
+    public K postId;
+    public V message;
+}
+
 public class ExampleStart {
+    public static void main(String[] args) {
+        Post<Short, String> post = new Post<Short, String>();
+
+    }
 }
