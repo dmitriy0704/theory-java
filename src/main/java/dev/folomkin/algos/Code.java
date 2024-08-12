cb36a5be8f18c435e0563a1c7d1a66138323e2af
Dmitriy Folomkin <dmitriy.folomkin@gmail.com>
2024-08-12 18:19:04 +0300

    Алгоритмы

---
+++ src/main/java/dev/folomkin/algos/Code.java
@@ -0,0 +1,12 @@
+package dev.folomkin.algos;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Code {
+    public static void main(String[] args) {
+
+    }
+}
