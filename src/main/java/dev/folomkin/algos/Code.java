460364ddc58adc70691f92ca68256c5a0b6b163a
Dmitriy Folomkin <dmitriy.folomkin@gmail.com>
2024-08-13 09:22:10 +0300

    Алгоритмы. Сортировка

--- src/main/java/dev/folomkin/algos/Code.java
+++ src/main/java/dev/folomkin/algos/Code.java
@@ -1,12 +1,31 @@
 package dev.folomkin.algos;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class Code {
     public static void main(String[] args) {
+        int[] testArr = new int[]{6, 3, 8, 2, 6, 9, 4, 11, 1};
+        bubbleSort(testArr);
+        System.out.println(Arrays.toString(testArr));
 
+    }
+
+    public static void bubbleSort(int[] arr) {
+        int tmp = 0, k = arr.length - 2;
+        boolean is_swap = false;
+        for (int i = 0; i <= k; i++) {
+            is_swap = false;
+            for (int j = k; j >= i; j--) {
+                if (arr[j] > arr[j + 1]) {
+                    tmp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = tmp;
+                    is_swap = true;
+                }
+            }
+
+            // Если перестановок не было, то выходим
+            if (is_swap == false) break;
+        }
     }
 }
