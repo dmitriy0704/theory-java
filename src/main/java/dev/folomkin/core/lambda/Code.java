e419f1c347c9ec96769dd39310445976446c0037
Dmitriy Folomkin <dmitriy.folomkin@gmail.com>
2024-08-23 18:09:01 +0300

    Лямбда-выражения. Ссылки на методы

--- src/main/java/dev/folomkin/core/lambda/Code.java
+++ src/main/java/dev/folomkin/core/lambda/Code.java
@@ -1,40 +1,22 @@
 package dev.folomkin.core.lambda;
 
+import java.io.IOException;
+import java.io.Reader;
 
-// Использование обобщенного функционального интерфейса;
-
-// Обобщенный функциональный интерфейс с двумя параметрами,
-// который возвращает результат типа boolean
-interface SomeTest<T> {
-    boolean test(T n, T m);
+// Демонстрация использования ссылки на метод экземпляра.
+// Функциональный интерфейс для предикатов,
+// работающих с целочисленными значениями,
+interface IntPredicate {
+    boolean test(int n);
 }
 
-public class Code {
-    public static void main(String[] args) {
+// Этот класс хранит значения типа int. Кроме того, в нем определен
+// метод экземпляра isFactor(), который возвращает true, если его аргумент
+// является делителем сохраненного числа
+class MyIntNum {
 
-        // Это лямбда выражение определяет, является
-        // ли одно число типа Integer делителем другого
-        SomeTest<Integer> testI = (n, d) -> (n % d) == 0;
-        if (testI.test(10, 2))
-            System.out.println("2 является делителем 10");
+}
 
-        // Это лямбда-выражение определяет, является ли одно число типа Double
-        // делителем другого
-        SomeTest<Double> testD = (n, d) -> (n % d) == 0;
-        if (testD.test(10.0, 2.0))
-            System.out.println("2 является делителем 10");
+class Code {
 
-
-        // Это лямбда-выражение определяет, является
-        // л и одна строка частью другой строки.
-        SomeTest<String> isIn = (a, b) -> a.indexOf(b) != -1;
-        String str = "Generic Functional Interface";
-
-        System.out.println("Проверяемая строка: " + str);
-        if (isIn.test(str, "face"))
-            System.out.println("Строка 'face' найдена.");
-        else
-            System.out.println("Строка 'face' не найдена.");
-
-    }
 }
