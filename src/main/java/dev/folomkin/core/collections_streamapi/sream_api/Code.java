a3ff8d0d29f6775dcda7d5048f9e89c6e852271a
Dmitriy Folomkin <dmitriy.folomkin@gmail.com>
2024-09-23 14:26:35 +0300

    Stream API

--- src/main/java/dev/folomkin/core/collections_streamapi/sream_api/Code.java
+++ src/main/java/dev/folomkin/core/collections_streamapi/sream_api/Code.java
@@ -1,15 +1,18 @@
 package dev.folomkin.core.collections_streamapi.sream_api;
 
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+
 
 
 public class Code {
     public static void main(String[] args) {
-        List<String> strings = List.of("Java Python Go JavaScript PH".split("\\s+"));
-        Map<Boolean, List<String>> boolLength = strings.stream()
-                .collect(Collectors.partitioningBy(s -> s.length() < 3));
-        System.out.println(boolLength);
+        List<Integer> strings = List.of(1,3, 4, 5);
+
+
+
+
     }
 }
