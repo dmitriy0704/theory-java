faaa90650745ca8a7194b26e0c79e6a4e1582fb1
Dmitriy Folomkin <dmitriy.folomkin@gmail.com>
2024-10-31 11:21:29 +0300

    Паттерны проектирования: Декоратор - готово

---
+++ src/main/java/dev/folomkin/design_patterns/patterns/gof/structural/decorator/Task.java
@@ -0,0 +1,11 @@
+package dev.folomkin.design_patterns.patterns.gof.structural.decorator;
+
+public class Task {
+    public static void main(String[] args) {
+        Developer developer = new JavaTeamLead(
+                new JavaSeniorDeveloper(new JavaDeveloper())
+        );
+
+        System.out.println(developer.makeJob());
+    }
+}
