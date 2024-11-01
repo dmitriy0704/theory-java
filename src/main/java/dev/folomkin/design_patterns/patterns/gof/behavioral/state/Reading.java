6feefc8a1cc0cbe3540bc5dbb76310c18c6b7043
Dmitriy Folomkin <dmitriy.folomkin@gmail.com>
2024-11-01 19:15:11 +0300

    Паттерны проектирования: Состояние - готово

---
+++ src/main/java/dev/folomkin/design_patterns/patterns/gof/behavioral/state/Reading.java
@@ -0,0 +1,8 @@
+package dev.folomkin.design_patterns.patterns.gof.behavioral.state;
+
+public class Reading implements Activity {
+    @Override
+    public void justDoIt() {
+        System.out.println("Reading book...");
+    }
+}
