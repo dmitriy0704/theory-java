c7e435112e6c61ec32de55e5aa3cbbdfa21321e6
Dmitriy Folomkin <dmitriy.folomkin@gmail.com>
2024-11-01 19:41:06 +0300

    Паттерны проектирования: Стратегия - готово

---
+++ src/main/java/dev/folomkin/design_patterns/patterns/gof/behavioral/strategy/Coding.java
@@ -0,0 +1,8 @@
+package dev.folomkin.design_patterns.patterns.gof.behavioral.strategy;
+
+public class Coding implements Activity {
+    @Override
+    public void justDoIt() {
+        System.out.println("Writing code...");
+    }
+}
---
+++ src/main/java/dev/folomkin/design_patterns/patterns/gof/behavioral/strategy/Training.java
@@ -0,0 +1,8 @@
+package dev.folomkin.design_patterns.patterns.gof.behavioral.strategy;
+
+public class Training implements Activity {
+    @Override
+    public void justDoIt() {
+        System.out.println("Training...");
+    }
+}
