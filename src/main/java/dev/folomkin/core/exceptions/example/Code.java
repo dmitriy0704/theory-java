a800fef9485d567f6b7a514cc53f0eee443c7a7f
Dmitriy Folomkin <dmitriy.folomkin@gmail.com>
2025-01-13 22:51:09 +0300

    Пролистаны исключения

--- src/main/java/dev/folomkin/core/exceptions/example/Code.java
+++ src/main/java/dev/folomkin/core/exceptions/example/Code.java
@@ -1,4 +1,16 @@
 package dev.folomkin.core.exceptions.example;
 
 public class Code {
+    public static void main(String[] args) {
+        int a, d;
+
+        try {
+            d = 0;
+            a = 42 / d;
+            System.out.println("Не выведется");
+        } catch (ArithmeticException e) {
+            System.out.println("Деление на ноль");
+        }
+        System.out.println("После catch block");
+    }
 }
