90d9263ba963e4566f20c0d28bb4fa09b65b2ccf
Dmitriy Folomkin <dmitriy.folomkin@gmail.com>
2025-01-28 14:19:17 +0300

    закончены коллекции

--- src/main/java/dev/folomkin/collection/code/Code.java
+++ src/main/java/dev/folomkin/collection/code/Code.java
@@ -2,48 +2,31 @@ package dev.folomkin.collection.code;
 
 import java.util.*;
 
-class MyComparator implements Comparator<String> {
-
-    @Override
-    public int compare(String o1, String o2) {
-        return o1.compareToIgnoreCase(o2);
-    }
-}
-
 public class Code {
     public static void main(String[] args) {
 
-        MyComparator comparator = new MyComparator();
+        Hashtable<String, Double> balance = new Hashtable<String, Double>();
+        Enumeration<String> names;
+        String str;
+        double bal;
+        balance.put("John Doe", 3434.34);
+        balance.put("Tom Smith", 123.22);
+        balance.put("Jane Baker", 1378.00);
+        balance.put("Tod Hall", 99.22);
+        balance.put("Ralph Smith", -19.08);
 
-        List<Integer> list = new LinkedList<>();
-        list.add(-8);
-        list.add(20);
-        list.add(-20);
-        list.add(8);
+        // Отобразить балансы всех счетов иэ хеш-таблицы.
 
-        // Создать компаратор с обратным порядком.
-        Comparator<Integer> r = Collections.reverseOrder();
-
-        // Сортировать список с использованием созданного компаратора.
-        Collections.sort(list, r);
-
-        System.out.print("Список отсортирован в обратном порядке: ");
-        for (int i : list) {
-            System.out.print(i + " ");
-        }
-
-        // Тасовать список.
-        Collections.shuffle(list);
-
-        // Отобразить рандомизированный список.
-        System.out.print("Список перетасован: ");
-        for (int i : list) {
-            System.out.print(i + " ");
+        Set<String> keys = balance.keySet();
+        Iterator<String> iterator = keys.iterator();
+        while (iterator.hasNext()) {
+            str = iterator.next();
+            System.out.println(str + ": " + balance.get(str));
         }
         System.out.println();
-        System.out.println(" Haимeньшee значение: " + Collections.min(list));
-        System.out.println("Haибoльшee значение: " + Collections.max(list));
-
+        bal = balance.get("John Doe");
+        balance.put("John Smith", bal + 1000);
+        System.out.println(bal);
 
     }
 }
