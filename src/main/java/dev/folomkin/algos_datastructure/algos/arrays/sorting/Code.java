ed567b975f8919636096b5ef0a4d09dfbd8b8a75
Dmitriy Folomkin <dmitriy.folomkin@yandex.ru>
2025-03-28 18:23:40 +0300

    proxy

--- src/main/java/dev/folomkin/algos_datastructure/algos/arrays/sorting/Code.java
+++ src/main/java/dev/folomkin/algos_datastructure/algos/arrays/sorting/Code.java
@@ -1,73 +1,36 @@
 package dev.folomkin.algos_datastructure.algos.arrays.sorting;
 
+import java.util.Arrays;
+
 public class Code {
-    // Метод для сортировки массива с использованием сортировки слиянием
-    public static void mergeSort(int[] array) {
-        if (array.length < 2) {
-            return; // Если массив состоит из одного элемента, он уже отсортирован
-        }
+    public static int[] bubbleSort(int[] array) {
+        int n = array.length;
+        boolean swapped;
 
-        int mid = array.length / 2; // Находим середину массива
-        int[] left = new int[mid]; // Создаем левую половину
-        int[] right = new int[array.length - mid]; // Создаем правую половину
-
-        // Заполняем левую и правую половины
-        for (int i = 0; i < mid; i++) {
-            left[i] = array[i];
-        }
-        for (int i = mid; i < array.length; i++) {
-            right[i - mid] = array[i];
-        }
-
-        // Рекурсивно сортируем обе половины
-        mergeSort(left);
-        mergeSort(right);
-
-        // Сливаем отсортированные половины
-        merge(array, left, right);
-    }
-
-    // Метод для слияния двух отсортированных массивов
-    private static void merge(int[] array, int[] left, int[] right) {
-        int i = 0, j = 0, k = 0;
-
-        // Сравниваем элементы левой и правой половин и заполняем основной массив
-        while (i < left.length && j < right.length) {
-            if (left[i] <= right[j]) {
-                array[k++] = left[i++];
-            } else {
-                array[k++] = right[j++];
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    swapped = true;
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+            if (!swapped) {
+                break;
             }
         }
-
-        // Копируем оставшиеся элементы левой половины, если есть
-        while (i < left.length) {
-            array[k++] = left[i++];
-        }
-
-        // Копируем оставшиеся элементы правой половины, если есть
-        while (j < right.length) {
-            array[k++] = right[j++];
-        }
+        return array;
     }
 
-    // Метод для вывода массива на экран
-    public static void printArray(int[] array) {
-        for (int value : array) {
-            System.out.print(value + " ");
-        }
-        System.out.println();
-    }
-
-    // Главный метод для тестирования сортировки слиянием
     public static void main(String[] args) {
-        int[] array = {38, 27, 43, 3, 9, 82, 10}; // Исходный массив
+        int[] array = {64, 34, 25, 12, 22, 11, 90};
         System.out.println("Исходный массив:");
-        printArray(array);
-
-        mergeSort(array); // Сортируем массив
-
+        System.out.println(Arrays.toString(array));
+        bubbleSort(array);
         System.out.println("Отсортированный массив:");
-        printArray(array); // Выводим отсортированный массив
+        int[] arr = bubbleSort(array);
+        System.out.println(Arrays.toString(arr));
     }
 }
