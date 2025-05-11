1511acffd593733178ec787c025b23836ba9f6a8
Dmitriy Folomkin <dmitriy.folomkin@yandex.ru>
2025-05-11 20:13:30 +0300

    qa/algos_datastructure/tree

--- src/main/java/dev/folomkin/pro/algos_datastructure/algos/arrays/search/Code.java
+++ src/main/java/dev/folomkin/pro/algos_datastructure/algos/arrays/search/Code.java
@@ -7,6 +7,7 @@ public class Code {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == target) {
                 return i;
+                // TODO
             }
         }
         return -1;
