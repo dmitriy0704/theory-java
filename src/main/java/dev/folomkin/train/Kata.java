0c205d52a9f8d38a6324128a71fd3e21784c3f8a
Dmitriy Folomkin <dmitriy.folomkin@yandex.ru>
2025-05-19 23:34:55 +0300

    Add testing code

---
+++ src/main/java/dev/folomkin/train/Kata.java
@@ -0,0 +1,7 @@
+package dev.folomkin.train;
+
+public class Kata {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/dev/folomkin/train/KataTest.java
@@ -0,0 +1,7 @@
+package dev.folomkin.train;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KataTest {
+
+}
